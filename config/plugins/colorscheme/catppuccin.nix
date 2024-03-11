b29505fcbab85f83bf11fe11535d020f7a84e79d
ikoxcw <78737594+ikoxcw@users.noreply.github.com>
2024-03-11 14:53:12 +0900

    Initial Commit

---
+++ config/plugins/colorscheme/catppuccin.nix
@@ -0,0 +1,11 @@
+{
+  colorschemes.catppuccin = {
+    enable = true;
+    integrations = {
+      cmp = true;
+      gitsigns = true;
+      nvimtree = true;
+      treesitter = true;
+    };
+  };
+}
